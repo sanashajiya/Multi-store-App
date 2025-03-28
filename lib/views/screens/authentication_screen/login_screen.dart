d6695bc157c4d87d19b69571ded0d37e97821075
sanashajiya <ssanashajiya@gmail.com>
2025-03-28 12:39:34 +0530

    Add validation to  Login screen

--- lib/views/screens/authentication_screen/login_screen.dart
+++ lib/views/screens/authentication_screen/login_screen.dart
@@ -4,176 +4,203 @@ import 'dart:ui';
 
 import 'package:multi_store_app/views/screens/authentication_screen/register_screen.dart';
 
-
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  // const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     backgroundColor: Colors.white.withAlpha((0.95 * 255).toInt()),
+      backgroundColor: Colors.white.withAlpha((0.95 * 255).toInt()),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Login Your Account",
-                style: GoogleFonts.getFont(
-                  "Lato",
-                  color: Color(0xFF0d120E),
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.2, 
-                  fontSize: 23,
-                  ),
-                ),
-                Text(
-                  "To Explore the world exclusives",style: GoogleFonts.getFont(
-                    'Lato',
-                    color: Color(0xFF0d120E),
-                    letterSpacing: 0.2, 
-                    fontSize: 14,
-                    ),
-                ),
-                Image.asset(
-                  'assets/images/illustration.png',
-                  width: 200,
-                  height: 200,
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    'Email',
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Login Your Account",
                     style: GoogleFonts.getFont(
-                      'Nunito Sans',
-                      fontWeight: FontWeight.w500,
+                      "Lato",
+                      color: Color(0xFF0d120E),
+                      fontWeight: FontWeight.bold,
                       letterSpacing: 0.2,
+                      fontSize: 23,
                     ),
                   ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(9),
-                      // borderSide: BorderSide(color: Colors.grey),
+                  Text(
+                    "To Explore the world exclusives",
+                    style: GoogleFonts.getFont(
+                      'Lato',
+                      color: Color(0xFF0d120E),
+                      letterSpacing: 0.2,
+                      fontSize: 14,
                     ),
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    labelText: 'Enter your email',
-                    labelStyle: GoogleFonts.getFont(
-                      'Nunito Sans',
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.1,
+                  ),
+                  Image.asset(
+                    'assets/images/illustration.png',
+                    width: 200,
+                    height: 200,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Email',
+                      style: GoogleFonts.getFont(
+                        'Nunito Sans',
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.2,
+                      ),
                     ),
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Image.asset(
-                        'assets/icons/email.png',
-                        width: 20,
-                        height: 20,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Email is required';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(9),
+                        // borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: 'Enter your email',
+                      labelStyle: GoogleFonts.getFont(
+                        'Nunito Sans',
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.1,
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/icons/email.png',
+                          width: 20,
+                          height: 20,
                         ),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(9),
-                      // borderSide: BorderSide(color: Colors.grey),
-                    ),
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    labelText: 'Enter your password',
-                    labelStyle: GoogleFonts.getFont(
-                      'Nunito Sans',
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.1,
-                    ),
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Image.asset(
-                        'assets/icons/password.png',
-                        width: 20,
-                        height: 20,
+                  SizedBox(height: 10),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter your password';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(9),
+                        // borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: 'Enter your password',
+                      labelStyle: GoogleFonts.getFont(
+                        'Nunito Sans',
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.1,
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/icons/password.png',
+                          width: 20,
+                          height: 20,
                         ),
+                      ),
+                      suffixIcon: Icon(Icons.visibility),
                     ),
-                    suffixIcon: Icon(Icons.visibility),
                   ),
-                ),
-                SizedBox(height: 20,),
-                Container(
-                  width: 319,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5 ),
-                    gradient: LinearGradient(colors: [Color(0xFF102DE1),Color(0xCC0D6EFF)]),
-                  ),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 278,
-                        top: 19,
-                        child: Opacity(
-                          opacity: 0.5,
-                          child: Container(
-                            width: 60,
-                            height: 60,
-                            clipBehavior: Clip.antiAlias,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 12,
-                                color: Color(0xFF103DE5),
+                  SizedBox(height: 20),
+                  InkWell(
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        print('pass');
+                      } else {
+                        print('failed');
+                      }
+                    },
+                    child: Container(
+                      width: 319,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF102DE1), Color(0xCC0D6EFF)],
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 278,
+                            top: 19,
+                            child: Opacity(
+                              opacity: 0.5,
+                              child: Container(
+                                width: 60,
+                                height: 60,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 12,
+                                    color: Color(0xFF103DE5),
+                                  ),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
                               ),
-                              borderRadius: BorderRadius.circular(30),
                             ),
                           ),
-                        )
-                      ),
-                      // Positioned(
-                      //   left: 260,
-                      //   top: 29,
-                      //   child: Opacity(
-                      //     opacity: 0.5,
-                      //     child: Container(
-                      //       width: 10,
-                      //       height: 10,
-
-                      //       clipBehavior: Clip.antiAlias,
-                      //       decoration: BoxDecoration(
-                      //         border: Border.all(width: 3),
-                      //         color: Color(0xFF2141E5),
-                      //         borderRadius: BorderRadius.circular(5),
-                      //       ),
-                      //     ),
-                      //     ),
-
-                      //   ),
-                        Positioned(
-                          left: 311,
-                          top: 36,
-                          child: Opacity(
-                            opacity: 0.3,
-                            child: Container(
-                              width: 5,
-                              height: 5,
-
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                // border: Border.all(width: 3),
-                                color: Color(0xFF2141E5),
-                                borderRadius: BorderRadius.circular(3),
+                          // Positioned(
+                          //   left: 260,
+                          //   top: 29,
+                          //   child: Opacity(
+                          //     opacity: 0.5,
+                          //     child: Container(
+                          //       width: 10,
+                          //       height: 10,
+            
+                          //       clipBehavior: Clip.antiAlias,
+                          //       decoration: BoxDecoration(
+                          //         border: Border.all(width: 3),
+                          //         color: Color(0xFF2141E5),
+                          //         borderRadius: BorderRadius.circular(5),
+                          //       ),
+                          //     ),
+                          //     ),
+            
+                          //   ),
+                          Positioned(
+                            left: 311,
+                            top: 36,
+                            child: Opacity(
+                              opacity: 0.3,
+                              child: Container(
+                                width: 5,
+                                height: 5,
+            
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                  // border: Border.all(width: 3),
+                                  color: Color(0xFF2141E5),
+                                  borderRadius: BorderRadius.circular(3),
+                                ),
                               ),
                             ),
-                            ),
-
-                        ),
-                        Positioned(
+                          ),
+                          Positioned(
                             left: 281,
                             top: -10,
                             child: Opacity(
@@ -187,8 +214,7 @@ class LoginScreen extends StatelessWidget {
                                   borderRadius: BorderRadius.circular(5),
                                 ),
                               ),
-                              ),
-
+                            ),
                           ),
                           Center(
                             child: Text(
@@ -199,44 +225,46 @@ class LoginScreen extends StatelessWidget {
                                 fontWeight: FontWeight.bold,
                                 letterSpacing: 0.2,
                               ),
-                        )
-                       ),
-
-                      ],
-                  )
-                ),
-                SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Need an Account?",
-                       style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w500,
-                        letterSpacing: 1,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    InkWell(
-                      onTap: (){
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterScreen(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'Sign Up',
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Need an Account?",
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterScreen(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'Sign Up',
                           style: GoogleFonts.roboto(
                             color: Color(0xFF103DE5),
                             fontWeight: FontWeight.bold,
                             letterSpacing: 1,
-                          )
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
-                )
-            ],
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),

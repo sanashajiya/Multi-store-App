9c0a28d6019382b8f0a7f2b61f3846bbb3d3fe02
sanashajiya <ssanashajiya@gmail.com>
2025-03-29 17:11:13 +0530

    serialize User Model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -20,5 +20,31 @@ class User {
     required this.password,
   });
 
+  // Serialization: convert User object to a Map
+  // Map: A Map is a collection of key/value pairs
+  // why: Converting to a map is an intermediate step that makes it easier to serialize
+  // the object to formates like json for storage or transmission.
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'fullName': fullName,
+      'email': email,
+      'state': state,
+      'city': city,
+      'locality': locality,
+      'password': password,
+    };
+  }
+
+  // Serialization : Map to a Json String
+  // This method directly encodes the data  from the Map into a Json String
+
+  // The json.encode() function converts a Dart object (such as Map or List)
+  // into a Json String representation, making it suitable for communication
+  // between different systems.
+
+  String toJson() => json.encode(toMap());
+
   
 }

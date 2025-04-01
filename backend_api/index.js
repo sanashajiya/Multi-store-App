b2c9933a09f5f2ec51ca024056861be2d350bff1
sanashajiya <ssanashajiya@gmail.com>
2025-04-01 19:37:50 +0530

    Add sub category Api Endpoint - Backend

--- backend_api/index.js
+++ backend_api/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const authRouter = require('./routes/auth');
 const bannerRouter = require('./routes/banner');
 const categoryRouter = require('./routes/category');
+const subCategoryRouter = require('./routes/sub_category');
 
 const PORT = 3000;
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(authRouter);
 app.use(bannerRouter);
 app.use(categoryRouter);
+app.use(subCategoryRouter);
 
 // Connect to MongoDB with error handling
 mongoose.connect(DB)
---
+++ backend_api/routes/sub_category.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const SubCategory = require('../models/sub_category');
+const subCategory = require('../models/sub_category');
+const subCategoryRouter = express.Router();
+
+subCategoryRouter.post('/api/subCategories', async (req, res)=>{
+    try {
+        const {categoryId, categoryName, image, subCategoryName} = req.body;
+        const subCategory = new SubCategory({categoryId, categoryName, image, subCategoryName});
+        await subCategory.save();
+        res.status(201).send(subCategory);
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+subCategoryRouter.get('/api/category/:categoryName/subCategories', async (req, res)=>{
+    try {
+        // extract the categoryName from the request Url using Destructing
+        const { categoryName } = req.params;
+        const subCategories = await SubCategory.find({categoryName:categoryName});
+
+        // check if any subcategory were found
+        if(!subCategories || subCategories.length === 0) {
+            // if no subcategories are found, response with a status code 404 error
+            return res.status(404).json({msg: "subcategories  not found"});
+        }
+        else{
+            // if subcategories are found, response with a status code 200 and the subcategories
+            return res.status(200).json(subCategories);
+        }
+    } catch (e) {
+        res.status(500).json({error:e.message})
+    }
+});
+module.exports = subCategoryRouter;

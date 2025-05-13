8a7a88b873a4b4c35a194cf0ef33f4f854070812
sanashajiya <ssanashajiya@gmail.com>
2025-05-13 20:48:28 +0530

    banners  with state management

---
+++ lib/providers/banner_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:smart_cart/models/banner_model.dart';
+
+class BannerProvider extends StateNotifier<List<BannerModel>> {
+  BannerProvider() : super([]);
+
+  void setBanners(List<BannerModel> banners) {
+    state = banners;
+  }
+}
+
+final bannerProvider =
+    StateNotifierProvider<BannerProvider, List<BannerModel>>((ref) => BannerProvider());

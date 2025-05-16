9607a3b6d07230be01f04a1767572bf2c8cd8cf4
sanashajiya <ssanashajiya@gmail.com>
2025-05-16 15:12:33 +0530

    Add Persistance to Wishlist and  Cart using SharedPreferences

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,5 +1,8 @@
 // import 'package:flutter/material.dart';
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_cart/models/cart.dart';
  
 // Define a StateNotifierProvider to expose an instance of the CartNotifier
@@ -12,7 +15,40 @@ final cartProvider =
 // A notifier class to manage the cart state, eextending stateNotifier
 // with an initial state of an empty app
 class CartNotifier extends StateNotifier<Map<String, Cart>> {
-  CartNotifier() : super({});
+  CartNotifier() : super({}){
+    _loadCartItems();
+  }
+
+
+  //A private method that loads items from sharedpreferences
+  Future<void> _loadCartItems() async {
+    //retrieving the sharepreferences instance to store data
+    final prefs = await SharedPreferences.getInstance();
+    //fetch the json string of the cart items from sharedpreferences  under the key cart_items
+    final cartString = prefs.getString('cart_items');
+    //checking if the string is not null, meaning there is  saved data to load
+    if (cartString != null) {
+      //decode the json String into map of dynamic data
+      final Map<String, dynamic> cartMap = jsonDecode(cartString);
+
+      //covert the dynamic map  into a map of Cart Object using the 'fromjson" factory method
+      final cartItems = cartMap
+          .map((key, value) => MapEntry(key, Cart.fromJson(value)));
+
+      //updating the state with the loaded cart items
+      state = cartItems;
+    }
+  }
+
+  //A private method that saves the current list of Cart items to sharedpreferences
+  Future<void> _saveCartItems() async {
+    //retrieving the sharepreferences instance to store data
+    final prefs = await SharedPreferences.getInstance();
+    //encoding the current state (Map of cart object ) into json String
+    final cartString = jsonEncode(state);
+    //saving the json string to sharedpreferences with the key "cart_items"
+    await prefs.setString('cart_items', cartString);
+  }
 
   //Method to add product to the cart
   void addProductToCart({
@@ -44,6 +80,7 @@ class CartNotifier extends StateNotifier<Map<String, Cart>> {
             description: state[productId]!.description,
             fullName: state[productId]!.fullName)
       };
+      _saveCartItems();
     } else {
       // if the product is not in the cart, add it with the provided details
       state = {
@@ -60,6 +97,7 @@ class CartNotifier extends StateNotifier<Map<String, Cart>> {
             description: description,
             fullName: fullName)
       };
+      _saveCartItems();
     }
   }
 
@@ -70,6 +108,7 @@ class CartNotifier extends StateNotifier<Map<String, Cart>> {
       // Notify the listeners that the state has changed
 
       state = {...state};
+      _saveCartItems();
     }
   }
 
@@ -80,6 +119,7 @@ class CartNotifier extends StateNotifier<Map<String, Cart>> {
 
       //Notify the listeners that the state has changed
       state = {...state};
+      _saveCartItems();
     }
   }
 
@@ -88,6 +128,7 @@ class CartNotifier extends StateNotifier<Map<String, Cart>> {
     state.remove(productId);
     // Notify Listeners that the state has changed
     state = {...state};
+    _saveCartItems();
   }
 
   // Method to calculate total amount of items we have inn cart

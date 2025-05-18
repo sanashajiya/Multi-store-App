7f63c0232c581d91f040f38fad617bb657013047
sanashajiya <ssanashajiya@gmail.com>
2025-05-18 09:01:56 +0530

    show top rated products using riverpod

--- lib/views/screens/nav_screens/home_screen.dart
+++ lib/views/screens/nav_screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:smart_cart/views/screens/nav_screens/widgets/category_item_widge
 import 'package:smart_cart/views/screens/nav_screens/widgets/header_widget.dart';
 import 'package:smart_cart/views/screens/nav_screens/widgets/popular_product_widget.dart';
 import 'package:smart_cart/views/screens/nav_screens/widgets/reusable_text_widget.dart';
+import 'package:smart_cart/views/screens/nav_screens/widgets/top_rated_product_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -21,6 +22,11 @@ class HomeScreen extends StatelessWidget {
               subtitle: 'View all'
             ),
             PopularProductWidget(),
+            ReusableTextWidget(
+              title: 'Top Rated Products', 
+              subtitle: 'View all'
+            ),
+            TopRatedProductWidget(),
           ],
         ),
       ),

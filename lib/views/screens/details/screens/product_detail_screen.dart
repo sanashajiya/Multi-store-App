5838ec5f37de368b785b4bd7d82787a95cc81203
sanashajiya <ssanashajiya@gmail.com>
2025-05-18 08:28:13 +0530

    show related products using riverpod

--- lib/views/screens/details/screens/product_detail_screen.dart
+++ lib/views/screens/details/screens/product_detail_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:smart_cart/controllers/product_controller.dart';
 import 'package:smart_cart/models/product.dart';
 import 'package:smart_cart/providers/cart_provider.dart';
 import 'package:smart_cart/providers/favorite_provider.dart';
+import 'package:smart_cart/providers/related_product_provider.dart';
 import 'package:smart_cart/services/manage_http_response.dart';
+import 'package:smart_cart/views/screens/nav_screens/widgets/product_item_widget.dart';
+import 'package:smart_cart/views/screens/nav_screens/widgets/reusable_text_widget.dart';
 
 class ProductDetailScreen extends ConsumerStatefulWidget {
   final Product product;
@@ -17,7 +21,26 @@ class ProductDetailScreen extends ConsumerStatefulWidget {
 
 class _ProductDetailScreenState extends ConsumerState<ProductDetailScreen> {
   @override
+  void initState() {
+    super.initState();
+    _fetchProduct();
+  }
+
+  Future<void> _fetchProduct() async {
+    final ProductController productController = ProductController();
+    try {
+      final products = await productController.loadRelatedProducts(
+        widget.product.id,
+      );
+      ref.read(relatedProductProvider.notifier).setProducts(products);
+    } catch (e) {
+      print('Error fetching products: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final relatedProduct = ref.watch(relatedProductProvider);
     final cartProviderObject = ref.read(cartProvider.notifier);
     final cartData = ref.watch(cartProvider);
     final isInCart = cartData.containsKey(widget.product.id);
@@ -62,143 +85,157 @@ class _ProductDetailScreenState extends ConsumerState<ProductDetailScreen> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Container(
-              width: 260,
-              height: 275,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(),
-              child: Stack(
-                clipBehavior: Clip.none,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                width: 260,
+                height: 275,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(),
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 50,
+                      child: Container(
+                        width: 260,
+                        height: 260,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFD8DDFF),
+                          borderRadius: BorderRadius.circular(130),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 22,
+                      top: 0,
+                      child: Container(
+                        width: 216,
+                        height: 274,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF9CA8FF),
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                        child: SizedBox(
+                          height: 300,
+                          child: PageView.builder(
+                            itemCount: widget.product.images.length,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Image.network(
+                                widget.product.images[index],
+                                width: 198,
+                                height: 225,
+                                fit: BoxFit.cover,
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Positioned(
-                    left: 0,
-                    top: 50,
-                    child: Container(
-                      width: 260,
-                      height: 260,
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFD8DDFF),
-                        borderRadius: BorderRadius.circular(130),
+                  Expanded(
+                    child: Text(
+                      overflow: TextOverflow.ellipsis,
+                      widget.product.productName,
+                      style: GoogleFonts.roboto(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1,
+                        color: Color(0xFF3C55Ef),
                       ),
                     ),
                   ),
-                  Positioned(
-                    left: 22,
-                    top: 0,
-                    child: Container(
-                      width: 216,
-                      height: 274,
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF9CA8FF),
-                        borderRadius: BorderRadius.circular(14),
-                      ),
-                      child: SizedBox(
-                        height: 300,
-                        child: PageView.builder(
-                          itemCount: widget.product.images.length,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context, index) {
-                            return Image.network(
-                              widget.product.images[index],
-                              width: 198,
-                              height: 225,
-                              fit: BoxFit.cover,
-                            );
-                          },
-                        ),
-                      ),
+                  SizedBox(width: 20),
+                  Text(
+                    "\$${widget.product.productPrice.toString()}",
+                    style: GoogleFonts.roboto(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF3C55Ef),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Text(
-                    overflow: TextOverflow.ellipsis,
-                    widget.product.productName,
-                    style: GoogleFonts.roboto(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1,
-                      color: Color(0xFF3C55Ef),
-                    ),
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                widget.product.category,
+                style: GoogleFonts.roboto(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
                 ),
-                SizedBox(width: 20),
-                Text(
-                  "\$${widget.product.productPrice.toString()}",
-                  style: GoogleFonts.roboto(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF3C55Ef),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              widget.product.category,
-              style: GoogleFonts.roboto(
-                color: Colors.grey,
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
               ),
             ),
-          ),
-          widget.product.totalRatings == 0
-              ? Text('')
-              : Padding(
-                padding: EdgeInsets.only(left: 8),
-                child: Row(
-                  children: [
-                    const Icon(Icons.star, color: Colors.amber),
-                    Text(
-                      widget.product.averageRating.toString(),
-                      style: GoogleFonts.montserrat(
-                        fontWeight: FontWeight.bold,
+            widget.product.totalRatings == 0
+                ? Text('')
+                : Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: Row(
+                    children: [
+                      const Icon(Icons.star, color: Colors.amber),
+                      Text(
+                        widget.product.averageRating.toString(),
+                        style: GoogleFonts.montserrat(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Text('(${widget.product.totalRatings.toString()})'),
-                  ],
-                ),
-              ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "About",
-                  style: GoogleFonts.lato(
-                    fontSize: 17,
-                    letterSpacing: 1.7,
-                    fontWeight: FontWeight.w700,
-                    color: Color(0xFF363330),
+                      Text('(${widget.product.totalRatings.toString()})'),
+                    ],
                   ),
                 ),
-                Text(
-                  widget.product.description,
-                  style: GoogleFonts.radioCanada(letterSpacing: 1),
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "About",
+                    style: GoogleFonts.lato(
+                      fontSize: 17,
+                      letterSpacing: 1.7,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF363330),
+                    ),
+                  ),
+                  Text(
+                    widget.product.description,
+                    style: GoogleFonts.radioCanada(letterSpacing: 1),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            ReusableTextWidget(title: "Related products", subtitle: ""),
+            SizedBox(
+              height: 250,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: relatedProduct.length,
+                  itemBuilder: (context, index) {
+                    final product = relatedProduct[index];
+                    return ProductItemWidget(product: product,);
+                  }),
+            ),
+            SizedBox(height: 60),
+          ],
+        ),
       ),
       bottomSheet: Padding(
         padding: EdgeInsets.all(8),
